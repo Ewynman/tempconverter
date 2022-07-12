c793f43bfaa7da9553e118f2039737eab590fe34
Eddie Wynman <92377881+Ewynman@users.noreply.github.com>
2022-07-12 10:10:39 -0400

    Update temp.c

--- temp.c
+++ temp.c
@@ -6,30 +6,57 @@ int main()
 	double Kelvin;
 	double Fahrenheit;
 	int numofSamples;
-
-	// Requests the expected melting tempature from the user
+	char UserChoice;
 	double temp;
-	printf("Enter the tempature at which you want to convert: ");
-	scanf("%lf", &temp);
 
-	// sets the values equal based on the given input and computes the calculations
-	Fahrenheit = (temp);
-	Celsius = ((temp - 32) * 5) / 9;
-	Kelvin = (Celsius + 273);
+	printf("What will your orginal tempature be in?\n");
+	printf("1 for Fahrenheit, 2 for Celsius, 3 for Kelvin\n");
+	scanf("%c", &UserChoice);
 
-	// prints out the sample number and the conversions on their own lines
-	printf("-------------------Sample %d-------------------\n", samplescompleted);
-	printf("The sample in Fahrenheit is: %0.3lf\n", Fahrenheit);
-	printf("The sample in Celsius is: %0.3lf\n", Celsius);
-	printf("The sample in Kelvin is: %0.3lf\n", Kelvin);
+	switch (UserChoice)
+	{
+	case '1':
 
-	// Clculates the average in the sample group and prints it out
-	double averageTmp;
-	averageTmp = (Celsius + Kelvin + Fahrenheit) / 3;
-	printf("The Average Tempature is: %.03lf\n", averageTmp);
+		printf("Enter the tempature at which you want to convert: ");
+		scanf("%lf", &temp);
 
-	// Calculates the overall average of all samples tested so far and prints it out
-	double overallAvg;
-	overallAvg += averageTmp / numofSamples;
-	printf("The overall average of all samples is: %0.3lf\n", overallAvg);
+		// sets the values equal based on the given input and computes the calculations
+		Fahrenheit = (temp);
+		Celsius = ((temp - 32) * 5) / 9;
+		Kelvin = (Celsius + 273);
+
+		// prints out the Orginal number and the conversions on their own lines
+		printf("The Orginal in Fahrenheit is: %0.3lf\n", Fahrenheit);
+		printf("The Orginal in Celsius is: %0.3lf\n", Celsius);
+		printf("The Orginal in Kelvin is: %0.3lf\n", Kelvin);
+		break;
+	case '2':
+		printf("Enter the tempature at which you want to convert: ");
+		scanf("%lf", &temp);
+
+		// sets the values equal based on the given input and computes the calculations
+		Fahrenheit = ((temp) * 9/5) + 32;
+		Celsius = (temp);
+		Kelvin = (Celsius + 273);
+
+		// prints out the Orginal number and the conversions on their own lines
+		printf("The Orginal in Fahrenheit is: %0.3lf\n", Fahrenheit);
+		printf("The Orginal in Celsius is: %0.3lf\n", Celsius);
+		printf("The Orginal in Kelvin is: %0.3lf\n", Kelvin);
+		break;
+	case '3':
+		printf("Enter the tempature at which you want to convert: ");
+		scanf("%lf", &temp);
+
+		// sets the values equal based on the given input and computes the calculations
+		Fahrenheit = (1.8*(temp-273)+32);
+		Celsius = (temp -273);
+		Kelvin = (temp);
+
+		// prints out the Orginal number and the conversions on their own lines
+		printf("The Orginal in Fahrenheit is: %0.3lf\n", Fahrenheit);
+		printf("The Orginal in Celsius is: %0.3lf\n", Celsius);
+		printf("The Orginal in Kelvin is: %0.3lf\n", Kelvin);
+		break;
+	}
 }
